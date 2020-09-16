113d8168071aa6f503c6975672fcbbc14eb5b217
Ahmed Mosaad <48773133+theMosaad@users.noreply.github.com>
2020-09-16 15:09:27 +0200

    add font-family class to parent div + add leading-none in examples

--- demo/components/Typography.js
+++ demo/components/Typography.js
@@ -6,8 +6,11 @@ export default function Typography() {
   return (
     <section>
       {Object.keys(fullConfig.theme.capsize.fontMetrics).map((fontFamily, fontFamilyIndex) => (
-        <div className="space-y-2 mb-12" key={fontFamilyIndex}>
-          <h2 className={`font-bold font-${fontFamily} text-gray-800`} style={{ fontSize: '5rem' }}>
+        <div className={`font-${fontFamily} space-y-2 mb-12`} key={fontFamilyIndex}>
+          <h2
+            className={`font-bold font-${fontFamily} leading-none text-gray-800`}
+            style={{ fontSize: '5rem' }}
+          >
             font-{fontFamily}
           </h2>
           {Object.keys(fullConfig.theme.fontSize)
@@ -15,13 +18,16 @@ export default function Typography() {
             .map((fontSize, fontSizeIndex) => (
               <div key={fontSizeIndex}>
                 <p
-                  className={`font-${fontFamily} text-${fontSize} text-gray-900 bg-red-200 mb-4`}
+                  className={`text-${fontSize} text-gray-900 bg-red-200 mb-4`}
                 >{`text-${fontSize}`}</p>
                 <p
-                  className={`font-${fontFamily} text-${fontSize} leading-tight text-gray-900 bg-red-200 mb-4`}
+                  className={`text-${fontSize} leading-none text-gray-900 bg-red-200 mb-4`}
+                >{`text-${fontSize} leading-none`}</p>
+                <p
+                  className={`text-${fontSize} leading-tight text-gray-900 bg-red-200 mb-4`}
                 >{`text-${fontSize} leading-tight`}</p>
                 <p
-                  className={`font-${fontFamily} text-${fontSize} leading-relaxed text-gray-900 bg-red-200 mb-4`}
+                  className={`text-${fontSize} leading-relaxed text-gray-900 bg-red-200 mb-4`}
                 >{`text-${fontSize} leading-relaxed`}</p>
               </div>
             ))}
